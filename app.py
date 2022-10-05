e1b40ea4ae5213aba3dafe1e30304ca1d92114eb
Ned Nadima <nnadima1@gmail.com>
2022-10-05 16:09:18 -0400

    Rolling dice

--- app.py
+++ app.py
@@ -1,30 +1,12 @@
-command = ""
-running = False
+import random
 
-while True:
-    command = input('> ').lower()
-    if command == 'start':
-        if running == True and command == 'start':
-            print('Car is already started!')
-        else:
-            print('The car is starting')
-            running = True
+class Dice:
+   def roll(self):
+       first = random.randint(1,6)
+       second = random.randint(1,6)
+       return first, second
 
-    elif command == 'stop':
-        if running == True and command == 'stop':
-            print('Stopping the car!')
-            running = False
-        else:
-            print('The car is already stopped')
-            running = False
-    elif command == 'help':
-        print('''
-Type Start to start the car
-Type Stop to stop the car
-Type Quit to quit the car
-        ''')
-    elif command == 'quit':
-        print('Quitting game, sorry to see you go!')
-        break
-    else:
-        print("Sorry we don't speak americano")
+
+
+dice = Dice()
+print(dice.roll())
